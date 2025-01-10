a0d1de7ca288018b884edd67c057e3fcb88033b8
mykhailovskiyd <delmoruch77@gmail.com>
2025-01-10 14:16:22 +0200

    gitignore+ update

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: true,
+  videoCompression: false,
+  defaultCommandTimeout: 5000,
+  pageLoadTimeout: 20000,
+  retries: {
+    runMode: 1,
+    openMode: 1
+  },
   e2e: {
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
